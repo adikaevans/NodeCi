8812c4af0d940c76d6d1a43aff7720995b17523e
adikaevans <adikaevans@gmail.com>
2022-06-18 13:08:12 +0300

    Image upload

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,5 +1,7 @@
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
+const uuid = require('uuid/v1') // Nothing special about v1
+const requireLogin = require('../middlewares/requireLogin');
 
 const s3 = new AWS.S3({ 
     accessKeyId: keys.accessKeyId,
@@ -7,5 +9,16 @@ const s3 = new AWS.S3({
 });
 
 module.exports = app => {
-    app.get('/api/upload', (req, res) => {});
+    app.get('/api/upload',requireLogin, (req, res) => {
+        const key = `${req.user.id}/${uuid()}.jpeg`;
+
+        s3.getSignedUrl('putObject', {  // putObject means upload a file in AWS S3.
+            Bucket: 'my-blog-bucket-123', // Sample bucket name
+            ContentType: 'jpeg',
+            Key: key
+        }, (err, url) => res.send({key, url}));
+     
+    });
 };
+
+/* We installed npm module(uuid) to generate a unique user identifier. It generates a string of random numbers and letters for use in uniquely renaming uploaded files to avoid accidental deletion or overwritting.*/
